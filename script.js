154a1acb3c11e6ec392564cdf0438549ef39fa17
Jorge Martinez <martinezjorge273@gmail.com>
2020-11-13 02:36:03 -0600

    Added a train() function that fits the model to the MNIST data set

--- script.js
+++ script.js
@@ -88,10 +88,58 @@ function getModel() {
     return model;
 }
 
+async function train(model, data) {
+    const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+    const container = {
+        name: 'Model Training',
+        tab: 'Model',
+        styles: {
+            height: '1000px'
+        }
+    };
+    const fitCallBacks = tfvis.show.fitCallbacks(container, metrics);
+
+    const BATCH_SIZE = 512;
+    const TRAIN_DATA_SIZE = 5500;
+    const TEST_DATA_SIZE = 1000;
+
+    const [trainXs, trainYs] = tf.tidy(() => {
+        const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+        return [
+            d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+            d.labels
+        ];
+    });
+
+    const [testXs, testYs] = tf.tidy(() => {
+        const d = data.nextTestBatch(TEST_DATA_SIZE);
+        return [
+            d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+            d.labels
+        ];
+    });
+
+    return model.fit(trainXs, trainYs, {
+        batchSize: BATCH_SIZE,
+        validationData: [testXs, testYs],
+        epochs: 10, 
+        shuffle: true,
+        callbacks: fitCallBacks
+    });
+}
+
 async function run() {
     const data = new MnistData();
     await data.load();
     await showExamples(data);
+
+    const model = getModel();
+    tfvis.show.modelSummary({
+                        name: 'Model Architecture',
+                        tab: 'Model'
+                    }, model);
+
+    await train(model, data);
 }
 
 document.addEventListener('DOMContentLoaded', run);
